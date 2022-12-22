b70edcc74ed7004422bacc2c0214d4c8ddffa451
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-22 18:16:50 -0300

    feat: updateProductAmount hook

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -100,9 +100,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) return;
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const updatedCart = [...cart];
+
+      const productExists = updatedCart.find(
+        (product) => product.id === productId
+      );
+
+      if (!productExists) throw Error();
+
+      productExists.amount = amount;
+      setCart(updatedCart);
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
